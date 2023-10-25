d003f3f7e91daf4a41a969af708846cb526a7e14
Remomare <75660801+Remomare@users.noreply.github.com>
2023-10-26 02:55:07 +0900

    add cnn

--- model/classifier.py
+++ model/classifier.py
@@ -1,1 +1,9 @@
 import torch
+
+class CNN(torch.nn.Module):
+    def __init__(self, in_channels: int, num_class: int, kernel_size: int = 3, stride: int = 1, padding: int = 1) -> None:
+        super(CNN, self).__init__()
+        
+        self.conv_layer_ls = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels, )
+        )
