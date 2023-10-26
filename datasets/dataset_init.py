5fcae98eb80fb8886870783a390dbbe5a9492435
Remomare <75660801+Remomare@users.noreply.github.com>
2023-10-26 13:59:03 +0900

    10261359

--- datasets/dataset_init.py
+++ datasets/dataset_init.py
@@ -2,7 +2,7 @@ from torch.utils.data import DataLoader
 
 from datasets.prepare_data import *
 
-def dataset_init(args, dataset_name:str) -> DataLoader:
+def get_dataset_loader(args, dataset_name:str) -> DataLoader:
     
     if dataset_name == "MNIST":
         return mnist_dataset(args)
